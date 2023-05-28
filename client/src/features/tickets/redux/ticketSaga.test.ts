5404b557da942ea2e7e62064ee9ec96fa5a975c4
Minh Nghia <wowminhnghia@gmail.com>
2023-05-28 15:13:51 +0700

    test network error on purchase will show toast and cancels transaction

--- client/src/features/tickets/redux/ticketSaga.test.ts
+++ client/src/features/tickets/redux/ticketSaga.test.ts
@@ -55,6 +55,7 @@ const holdAction = {
 const networkProviders: Array<StaticProvider> = [
   [matchers.call.fn(reserveTicketServerCall), null],
   [matchers.call.fn(releaseServerCall), null],
+  [matchers.call.fn(cancelPurchaseServerCall), null],
 ];
 
 test("cancelTransaction cancels hold and resets transaction", () => {
@@ -68,16 +69,18 @@ test("cancelTransaction cancels hold and resets transaction", () => {
 describe("common to all flows", () => {
   test("starts with hold call to server", () => {
     // expectSaga is asynchronous fn so we have to use return keyword or async await
-    return expectSaga(ticketFlow, holdAction)
-      .provide(networkProviders)
-      .dispatch(
-        startTicketAbort({
-          reservation: holdReservation,
-          reason: "Abort request",
-        })
-      )
-      .call(reserveTicketServerCall, holdReservation)
-      .run();
+    return (
+      expectSaga(ticketFlow, holdAction)
+        .provide(networkProviders)
+        .dispatch(
+          startTicketAbort({
+            reservation: holdReservation,
+            reason: "Abort request",
+          })
+        )
+        // .call(reserveTicketServerCall, holdReservation)
+        .run()
+    );
   });
   test("show error toast and clean up after server error", () => {
     return (
@@ -106,5 +109,34 @@ describe("common to all flows", () => {
         .call(cancelTransaction, holdReservation)
         .run()
     );
+  });
+});
+
+describe("purchase flow", () => {
+  test("network error on purchase shows toast and cancels transaction", () => {
+    return expectSaga(ticketFlow, holdAction)
+      .provide([
+        [
+          matchers.call.like({
+            fn: reserveTicketServerCall,
+            args: [purchaseReservation], // not holdReservation
+          }),
+          throwError(new Error(`it did not work!`)),
+        ],
+        [
+          matchers.select.selector(selectors.getTicketAction),
+          TicketAction.hold,
+        ],
+        ...networkProviders,
+      ])
+      .dispatch(startTicketPurchase(purchasePayload))
+      .call(cancelPurchaseServerCall, purchaseReservation)
+      .put(
+        showToast(
+          generateErrorToastOptions(`it did not work!`, TicketAction.hold)
+        )
+      )
+      .call(cancelTransaction, holdReservation)
+      .run();
   });
 });
