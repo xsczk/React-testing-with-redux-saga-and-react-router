384a5d9949afc53a4e53b212776f43fb501312ad
Minh Nghia <wowminhnghia@gmail.com>
2023-05-29 13:32:08 +0700

    test for successful sign-in flow

--- client/src/features/auth/redux/signInSaga.test.ts
+++ client/src/features/auth/redux/signInSaga.test.ts
@@ -14,9 +14,35 @@ import {
 } from "./authSlice";
 import { expectSaga } from "redux-saga-test-plan";
 import { authenticateUser, signInFlow } from "./signInSaga";
+import * as matchers from "redux-saga-test-plan/matchers";
+import { StaticProvider } from "redux-saga-test-plan/providers";
+
+const signInRequestPayload: SignInDetails = {
+  email: "wowminhnghia@gmail.com",
+  password: "12021999",
+  action: "signIn",
+};
+
+const networkProviders: Array<StaticProvider> = [
+  [matchers.call.fn(authServerCall), null],
+];
 
 describe("signInFlow saga", () => {
-  test.todo("successfull sign-in");
+  test("successfull sign-in", () => {
+    return (
+      expectSaga(signInFlow)
+        .provide(networkProviders)
+        .dispatch(signInRequest(signInRequestPayload))
+        .fork(authenticateUser, signInRequestPayload)
+        .put(startSignIn())
+        // TODO: create provider
+        .call(authServerCall, signInRequestPayload)
+        .put.actionType(signIn.type) // partial assertion
+        .put.actionType(showToast.type)
+        .put(endSignIn())
+        .silentRun() // using silentRun to avoid warning timeout in infinite loop
+    );
+  });
   test.todo("successfull sign-up");
   test.todo("cancelled sign-in");
   test.todo("sign-in error");
