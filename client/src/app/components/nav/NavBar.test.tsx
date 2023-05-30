41299d08fd4f3228dff26eca43c574311fb8b3eb
Minh Nghia <wowminhnghia@gmail.com>
2023-05-30 13:38:48 +0700

    display in navigation  when user sign in / not sign in

--- client/src/app/components/nav/NavBar.test.tsx
+++ client/src/app/components/nav/NavBar.test.tsx
@@ -3,22 +3,47 @@ import { NavBar } from "./NavBar";
 import userEvent from "@testing-library/user-event";
 import { App } from "../../../App";
 
-test("clicking sign-in button pushs 'sign-in' route to history", async () => {
-  const user = userEvent.setup();
-  const { history } = render(<NavBar />);
-  const signInButton = screen.getByRole("button", { name: /sign in/i });
-  await user.click(signInButton);
-  expect(history.location.pathname).toBe("/signin");
+describe("sign-in button navigation", () => {
+  test("clicking sign-in button pushs 'sign-in' route to history", async () => {
+    const user = userEvent.setup();
+    const { history } = render(<NavBar />);
+    const signInButton = screen.getByRole("button", { name: /sign in/i });
+    await user.click(signInButton);
+    expect(history.location.pathname).toBe("/signin");
+  });
+
+  test("clicking sign-in button shows sign-in page", async () => {
+    const user = userEvent.setup();
+    render(<App />);
+    const signInButton = screen.getByRole("button", { name: /sign in/i });
+    await user.click(signInButton);
+    expect(
+      screen.getByRole("heading", {
+        name: /sign in to your account/i,
+      })
+    ).toBeInTheDocument();
+  });
 });
 
-test("clicking sign-in button shows sign-in page", async () => {
-  const user = userEvent.setup();
-  render(<App />);
-  const signInButton = screen.getByRole("button", { name: /sign in/i });
-  await user.click(signInButton);
-  expect(
-    screen.getByRole("heading", {
-      name: /sign in to your account/i,
-    })
-  ).toBeInTheDocument();
+describe("display when signed in / not signed in", () => {
+  test("should display sign in button when user is null", () => {
+    render(<NavBar />);
+    const signInButton = screen.getByRole("button", {
+      name: /sign in/i,
+    });
+    expect(signInButton).toBeInTheDocument();
+  });
+
+  test("should display sign out button and user email when user is not null", () => {
+    render(<NavBar />, {
+      preloadedState: {
+        user: { userDetails: { email: "wowminhnghia@gmail.com" } },
+      },
+    });
+    const signOuntButton = screen.getByRole("button", {
+      name: /sign out/i,
+    });
+    expect(signOuntButton).toBeInTheDocument();
+    expect(screen.getByText(/wowminhnghia@gmail.com/)).toBeInTheDocument();
+  });
 });
