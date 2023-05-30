af542e25b100329ab378d23d63dc04e05b469eea
Minh Nghia <wowminhnghia@gmail.com>
2023-05-30 10:37:34 +0700

    behavior testing for test case: redirect to signin if no user logged in

--- client/src/features/auth/components/UserProfile.test.tsx
+++ client/src/features/auth/components/UserProfile.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "../../../test-utils";
 import { UserProfile } from "./UserProfile";
+import { App } from "../../../App";
 
 const testUser = {
   email: "wowminhnghia@gmail.com",
@@ -16,4 +17,13 @@ test("redirects to sign-in if user is falsy", () => {
   const { history } = render(<UserProfile />);
   expect(history.location.pathname).toBe("/signin");
   expect(screen.queryByText(/hi/i)).not.toBeInTheDocument();
+});
+
+test("view sign-in page when loading profile while not logged in", () => {
+  // leave the preloadedState blank so that user is undefined
+  render(<App />, { routeHistory: ["/profile"] });
+  const heading = screen.getByRole("heading", {
+    name: /sign in to your account/i,
+  });
+  expect(heading).toBeInTheDocument();
 });
